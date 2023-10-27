951cf811aad3bf517d1366a1431a43047602779b
George Freeman <65429503+George-Freeman-gh@users.noreply.github.com>
2023-10-27 13:35:09 -0400

    second attempt to remove double tap zoom
    touch-action: manipulation; in css did not prevent the double tap zoom. I am trying a javascript method to prevent double tap zoom

--- index.js
+++ index.js
@@ -160,6 +160,16 @@ document.addEventListener('click', (e) => {
     })
    
 
+    // code to prevent double tap zoom on mobile devices 
+
+    let lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+    const now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+    }
+    lastTouchEnd = now;
+}, false);
   
 
   
